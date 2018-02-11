7a800ed3a14d399c38f9e7cacecf201de269bc1b
annishelton <36040710+annishelton@users.noreply.github.com>
2018-02-11 15:03:54 -0500

    added shake photo

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,37 @@
-var count = 1;
+(function($){
+    $.fn.bounce = function(settings) {
+        if(typeof settings.interval == 'undefined'){
+            settings.interval = 100;
+        }
 
-$('.counterincrementbutton').click(function() {count = count + 1;
-  $('.currentcount').text(count)
-})
+        if(typeof settings.distance == 'undefined'){
+            settings.distance = 10;
+        }
+
+        if(typeof settings.times == 'undefined'){
+            settings.times = 4;
+        }
+
+        if(typeof settings.complete == 'undefined'){
+            settings.complete = function(){};
+        }
+
+        $(this).css('position','relative');
+
+        for(var iter=0; iter<(settings.times+1); iter++){
+            $(this).animate({ top:((iter%2 == 0 ? settings.distance : settings.distance * -1)) }, settings.interval);
+        }
+
+        $(this).animate({ top: 0}, settings.interval, settings.complete);
+    };
+})(jQuery);
+
+$(document).ready(function(){
+    $('button.bounce').click(function(e){
+        $(this).bounce({
+            interval: 100,
+            distance: 20,
+            times: 5
+        });
+    });
+});
